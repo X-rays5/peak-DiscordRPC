7b4abf3f7063642d36b325ebbb7a3918d1166775
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2025-08-14 21:09:44 +0200

    Fix spamming if RPC updates

--- PeakDiscordRPC/CharacterUpdateHook.cs
+++ PeakDiscordRPC/CharacterUpdateHook.cs
@@ -5,11 +5,17 @@ namespace PeakDiscordRPC;
 [HarmonyPatch(typeof(Character), "Update")]
 public class CharacterUpdateHook
 {
+    private static float _lastUpdateTime;
+
     static void Postfix(Character __instance)
     {
         if (!__instance.IsLocal)
             return;
 
+        if (UnityEngine.Time.time - _lastUpdateTime < 5f)
+            return;
+
+        _lastUpdateTime = UnityEngine.Time.time;
         DiscordRichPresence.PresenceUpdate();
     }
 }
